3709e9d570bc2b50cbc9d6b1031838da3a9df446
AdrisJr <149505400+AdrisJr@users.noreply.github.com>
2025-08-20 21:08:58 -0300

    Readme
    Adicionada uma descrição e tirei umas quebras de linha do código

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # pete-o-padeiro
 Desafio em php
+
+testado usando a versão do PHP 8.4.11, rodando localmente no Visual Studio Code.
--- script.php
+++ script.php
@@ -19,11 +19,11 @@ var_dump(cakes(
   === 2); 
 
 var_dump(cakes(
-  ['apples' => 3, 'flour' => 300, 'sugar' => 150, 'milk' => 100, 'oil' => 100], ['sugar' => 500, 'flour' => 2000, 'milk' => 2000]) 
+  ['apples' => 3, 'flour' => 300, 'sugar' => 150, 'milk' => 100, 'oil' => 100],
+  ['sugar' => 500, 'flour' => 2000, 'milk' => 2000]) 
   === 0); 
 
 var_dump(cakes(
   ['flour' => 500, 'sugar' => 200, 'eggs' => 1], 
   ['flour' => 1500, 'sugar' => 1200, 'eggs' => 5, 'milk' => 200]) 
   === 3); 
-
